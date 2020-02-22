e21094829b36fa60135714a210ec3c0b61cb1439
Jesper Skand <jesper.skand@gmail.com>
2020-02-22 12:48:01 +0200

    Change port number

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const express = require("express");
 var cors = require('cors')
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const API_PORT = process.env.PORT || 3001
+const API_PORT = process.env.PORT || 3000
 const app = express()
 const gigantti = require("./routes/gigantti/index.js");
 
